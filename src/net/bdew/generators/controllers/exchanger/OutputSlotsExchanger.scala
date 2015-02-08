964532c35539397f107b7e9112362a97f6d6797c
bdew <blackdew@gmail.com>
2015-02-08 23:40:48 +0200

    updated OutputSlotsExchanger to new format (#23)

--- src/net/bdew/generators/controllers/exchanger/OutputSlotsExchanger.scala
+++ src/net/bdew/generators/controllers/exchanger/OutputSlotsExchanger.scala
@@ -14,9 +14,15 @@ import net.bdew.lib.gui.Texture
 import net.bdew.lib.multiblock.data.SlotSet
 
 object OutputSlotsExchanger extends SlotSet("fluidslots_exchanger") {
-  val BOTH = Slot("BOTH", "advgenerators.exchanger.output.both", Texture(Textures.sheet, 101, 35, 14, 14))
-  val COLD = Slot("COLD", "advgenerators.exchanger.output.cold", Texture(Textures.sheet, 133, 35, 14, 14))
-  val HOT = Slot("HOT", "advgenerators.exchanger.output.hot", Texture(Textures.sheet, 117, 35, 14, 14))
+  val BOTH = Slot("BOTH", "advgenerators.exchanger.output.both")
+  val COLD = Slot("COLD", "advgenerators.exchanger.output.cold")
+  val HOT = Slot("HOT", "advgenerators.exchanger.output.hot")
   override def default = BOTH
   override val order = Map(BOTH -> COLD, COLD -> HOT, HOT -> BOTH)
+
+  override lazy val textures = Map(
+    BOTH -> Texture(Textures.sheet, 101, 35, 14, 14),
+    COLD -> Texture(Textures.sheet, 133, 35, 14, 14),
+    HOT -> Texture(Textures.sheet, 117, 35, 14, 14)
+  )
 }
