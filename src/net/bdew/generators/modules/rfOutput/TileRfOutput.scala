ddbe276e61792726152144f90e6df0b7d98cdb18
bdew <blackdew@gmail.com>
2015-02-24 14:21:44 +0200

    RF output should check for IEnergyReceiver instead of IEnergyHandler

--- src/net/bdew/generators/modules/rfOutput/TileRfOutput.scala
+++ src/net/bdew/generators/modules/rfOutput/TileRfOutput.scala
@@ -9,7 +9,7 @@
 
 package net.bdew.generators.modules.rfOutput
 
-import cofh.api.energy.IEnergyHandler
+import cofh.api.energy.{IEnergyHandler, IEnergyReceiver}
 import net.bdew.generators.config.Tuning
 import net.bdew.lib.multiblock.data.OutputConfigPower
 import net.bdew.lib.multiblock.interact.CIPowerProducer
@@ -38,7 +38,7 @@ class TileRfOutput extends TileOutput[OutputConfigPower] with RSControllableOutp
   override def doOutput(face: ForgeDirection, cfg: OutputConfigPower) {
     getCoreAs[CIPowerProducer] map { core =>
       val out = if (checkCanOutput(cfg)) {
-        myPos.neighbour(face).getTile[IEnergyHandler](worldObj) map { tile =>
+        myPos.neighbour(face).getTile[IEnergyReceiver](worldObj) map { tile =>
           val canExtract = core.extract(Int.MaxValue, true)
           val injected = tile.receiveEnergy(face.getOpposite, (canExtract * ratio).toInt, false)
           core.extract(injected / ratio, false)
