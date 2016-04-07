f9327e8a866eea67c9e34da4c55329d09861f987
bdew <blackdew@gmail.com>
2016-04-07 16:37:30 +0300

    updated forge, restored WAILA integration

---
+++ src/net/bdew/generators/waila/FuelTurbineDataProvider.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) bdew, 2015
+ * https://github.com/bdew/generators
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://bdew.net/minecraft-mod-public-license
+ */
+
+package net.bdew.generators.waila
+
+import net.bdew.generators.config.Config
+import net.bdew.generators.controllers.turbine.TileTurbineController
+import net.bdew.lib.{DecFormat, Misc}
+
+object FuelTurbineDataProvider extends BaseControllerDataProvider(classOf[TileTurbineController]) {
+  override def getBodyStringsFromTE(te: TileTurbineController) = {
+    List(
+      if (te.fuel.getFluid == null) {
+        Misc.toLocal("advgenerators.waila.turbine.nofuel")
+      } else {
+        "%s/%s mB %s".format(DecFormat.round(te.fuel.getFluidAmount), DecFormat.round(te.fuel.getCapacity), te.fuel.getFluid.getLocalizedName)
+      },
+      "%s/%s %s".format(DecFormat.round(te.power.stored * Config.powerShowMultiplier), DecFormat.round(te.power.capacity * Config.powerShowMultiplier), Config.powerShowUnits),
+      Misc.toLocalF("advgenerators.waila.turbine.consuming", DecFormat.short(te.fuelPerTickAverage.average), "mB"),
+      Misc.toLocalF("advgenerators.waila.turbine.producing", DecFormat.short(te.outputAverage.average * Config.powerShowMultiplier), Config.powerShowUnits)
+    )
+  }
+}
---
+++ src/net/bdew/generators/waila/SyngasProducerDataProvider.scala
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) bdew, 2015
+ * https://github.com/bdew/generators
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://bdew.net/minecraft-mod-public-license
+ */
+
+package net.bdew.generators.waila
+
+import net.bdew.generators.config.Blocks
+import net.bdew.generators.controllers.syngas.TileSyngasController
+import net.bdew.lib.{DecFormat, Misc}
+import net.minecraftforge.fluids.FluidRegistry
+
+object SyngasProducerDataProvider extends BaseControllerDataProvider(classOf[TileSyngasController]) {
+  override def getBodyStringsFromTE(te: TileSyngasController) = {
+    List(
+      "%s/%s mB %s".format(DecFormat.round(te.waterTank.getFluidAmount), DecFormat.round(te.waterTank.getCapacity), FluidRegistry.WATER.getLocalizedName(null)),
+      "%s/%s mB %s".format(DecFormat.round(te.syngasTank.getFluidAmount), DecFormat.round(te.syngasTank.getCapacity), Blocks.syngasFluid.getLocalizedName(null)),
+      Misc.toLocalF("advgenerators.label.syngas.heat", DecFormat.round(te.heat), DecFormat.round(te.cfg.maxHeat)),
+      Misc.toLocalF("advgenerators.label.syngas.steam", DecFormat.short(te.steamBuffer / te.cfg.internalTankCapacity * 100) + "%"),
+      Misc.toLocalF("advgenerators.label.syngas.carbon", DecFormat.short(te.carbonBuffer / te.cfg.internalTankCapacity * 100) + "%")
+    )
+  }
+}
