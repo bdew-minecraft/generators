398c368275e0976f4de874b8b331c3a17dad6c90
bdew <blackdew@gmail.com>
2022-03-27 19:01:55 +0300

    updated fluid input/output to use new right click code from bdlib

--- src/main/scala/net/bdew/generators/modules/fluidInput/BlockFluidInput.scala
+++ src/main/scala/net/bdew/generators/modules/fluidInput/BlockFluidInput.scala
@@ -2,41 +2,19 @@ package net.bdew.generators.modules.fluidInput
 
 import net.bdew.generators.modules.BaseModule
 import net.bdew.generators.registries.Modules
-import net.bdew.lib.capabilities.Capabilities
 import net.bdew.lib.capabilities.helpers.FluidHelper
 import net.minecraft.core.BlockPos
-import net.minecraft.sounds.SoundEvents
-import net.minecraft.world.{InteractionHand, InteractionResult}
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.state.BlockState
 import net.minecraft.world.phys.BlockHitResult
-import net.minecraftforge.fluids.FluidStack
-import net.minecraftforge.fluids.capability.IFluidHandler.FluidAction
+import net.minecraft.world.{InteractionHand, InteractionResult}
 
 class BlockFluidInput extends BaseModule[TileFluidInput](Modules.fluidInput) {
   override def use(state: BlockState, world: Level, pos: BlockPos, player: Player, hand: InteractionHand, hit: BlockHitResult): InteractionResult = {
     if (!world.isClientSide) {
-      val item = player.getItemInHand(hand)
-      if (!item.isEmpty) {
-        val didSomething = item.copy().getCapability(Capabilities.CAP_FLUID_HANDLER_ITEM).map[Boolean](cap => {
-          val fluid = cap.drain(Int.MaxValue, FluidAction.SIMULATE)
-          if (!fluid.isEmpty) {
-            val moved = getTE(world, pos).getCapability(Capabilities.CAP_FLUID_HANDLER).map[FluidStack](ourCap => {
-              FluidHelper.pushFluid(cap, ourCap)
-            }).orElse(FluidStack.EMPTY)
-            if (moved.isEmpty) {
-              false
-            } else {
-              world.playSound(null, pos, SoundEvents.BUCKET_EMPTY, player.getSoundSource, 1, 1)
-              if (!player.isCreative)
-                player.setItemInHand(hand, cap.getContainer)
-              true
-            }
-          } else false
-        }).orElse(false)
-        if (didSomething) return InteractionResult.CONSUME
-      }
+      if (FluidHelper.blockFluidInteract(world, pos, player, hand))
+        return InteractionResult.CONSUME
     }
     super.use(state, world, pos, player, hand, hit)
   }
--- src/main/scala/net/bdew/generators/modules/fluidOutputSelect/BlockFluidOutputSelect.scala
+++ src/main/scala/net/bdew/generators/modules/fluidOutputSelect/BlockFluidOutputSelect.scala
@@ -2,12 +2,24 @@ package net.bdew.generators.modules.fluidOutputSelect
 
 import net.bdew.generators.modules.BaseModule
 import net.bdew.generators.registries.Modules
+import net.bdew.lib.capabilities.helpers.FluidHelper
 import net.bdew.lib.multiblock.block.BlockOutput
 import net.bdew.lib.multiblock.misc.BlockForcedOutput
 import net.minecraft.core.{BlockPos, Direction}
-import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.{BlockGetter, Level}
+import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.{InteractionHand, InteractionResult}
 
 class BlockFluidOutputSelect extends BaseModule[TileFluidOutputSelect](Modules.fluidOutput) with BlockOutput[TileFluidOutputSelect] with BlockForcedOutput {
   override def canConnectRedstone(state: BlockState, world: BlockGetter, pos: BlockPos, side: Direction): Boolean = true
+
+  override def use(state: BlockState, world: Level, pos: BlockPos, player: Player, hand: InteractionHand, hit: BlockHitResult): InteractionResult = {
+    if (!world.isClientSide) {
+      if (FluidHelper.blockFluidInteract(world, pos, player, hand))
+        return InteractionResult.CONSUME
+    }
+    super.use(state, world, pos, player, hand, hit)
+  }
 }
