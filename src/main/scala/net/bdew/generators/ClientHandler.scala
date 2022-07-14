0bba224467aa0e72de3f86c547ad115c4a67ea04
bdew <blackdew@gmail.com>
2022-07-14 22:08:07 +0300

    remove render type from MultiblockRenderHelper and change color registration to event

--- src/main/scala/net/bdew/generators/ClientHandler.scala
+++ src/main/scala/net/bdew/generators/ClientHandler.scala
@@ -5,6 +5,7 @@ import net.bdew.lib.multiblock.render.MultiblockRenderHelper
 import net.minecraft.client.renderer.{ItemBlockRenderTypes, RenderType}
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.client.event.ModelEvent.RegisterGeometryLoaders
+import net.minecraftforge.client.event.RegisterColorHandlersEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus
@@ -18,10 +19,14 @@ object ClientHandler {
   }
 
   @SubscribeEvent
+  def registerBlockColors(ev: RegisterColorHandlersEvent.Block): Unit = {
+    MultiblockRenderHelper.setupColors(ev, Blocks.all.map(_.get()), GeneratorsResourceProvider)
+  }
+
+  @SubscribeEvent
   def clientSetup(ev: FMLClientSetupEvent): Unit = {
     ev.enqueueWork(new Runnable {
       override def run(): Unit = {
-        MultiblockRenderHelper.setup(Blocks.all.map(_.get()), GeneratorsResourceProvider)
         ItemBlockRenderTypes.setRenderLayer(Fluids.syngas.source.get(), RenderType.translucent())
         ItemBlockRenderTypes.setRenderLayer(Fluids.syngas.flowing.get(), RenderType.translucent())
       }
